b7b453175252d79b8dcb6988bee98e1744659da5
mario <mariomottl17@gmail.com>
2024-12-18 20:39:05 +0100

    Setup day19

---
+++ day19/src/bin/part2.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+use std::fs::File;
+use std::io::Read;
+
+/*
+https://adventofcode.com/2024/day/19
+*/
+
+pub fn read_input(file_path: &str) -> Result<String> {
+    let mut file = File::open(file_path).expect("File not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Error reading file");
+    Ok(contents)
+}
+
+#[allow(dead_code)]
+const INPUT_FILE: &str = "input.txt";
+#[allow(dead_code)]
+const EXAMPLE_FILE: &str = "example.txt";
+
+fn main() {
+    #[allow(unused_variables)]
+    let contents: String = read_input(INPUT_FILE).unwrap_or_else(|err| panic!("{}", err));
+}
